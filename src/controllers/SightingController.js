f5a474f54f652c8c634a7899f5f21fc3d32f8545
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-11-04 12:03:53 +1100

    feat: delete operation

--- src/controllers/SightingController.js
+++ src/controllers/SightingController.js
@@ -105,13 +105,14 @@ router.patch("/:id", async (request, response) => {
 // Find one cat by its ID,
 // and delete it from the DB.
 router.delete("/:id", async (request, response) => {
-	let result = null;
+	let result = await Sighting.findByIdAndDelete(request.params.id).populate('user cats', '-password');
 
 	response.json({
-		cat: result
+		deletedSighting: result
 	});
 
 });
+
 
 
 module.exports = router;
